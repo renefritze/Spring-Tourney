fcc7d72aadb0f291948c1e750cff3b74840e31ab
Rene Milk <koshi@springlobby.info>
2010-06-07 01:18:22 +0200

    use decorator for a func with extra arg

--- profile.py
+++ profile.py
@@ -1,31 +1,27 @@
 # -*- coding: utf-8 -*-
-from bottle import route,request,send_file
+from bottle import request,send_file,route
 from siteglobals import env, db, config
 from utils import *
 from backend import Player,ElementNotFoundException
+from decorators import saferoute
 
 @route('/tourneys/:filename')
 def tourney_js(filename):
 	return send_file( filename, root=os.getcwd()+'/tourneys/' )
 
-@route('/profile/:id', method='GET')
-def output(id=0):
+@saferoute('/profile/:id', method='GET')
+def output(session, id=0):
+	if not id:
+		raise ElementNotFoundException( id )
 	try:
-		session = db.sessionmaker()
-		if not id:
-			raise ElementNotFoundException( id )
-		try:
-			player = session.query( Player ).filter( Player.id == id ).one()
+		player = session.query( Player ).filter( Player.id == id ).one()
+	except:
+		try: 
+			player = session.query( Player ).filter( Player.nick == id ).one()
 		except:
-			try: 
-				player = session.query( Player ).filter( Player.nick == id ).one()
-			except:
-				raise ElementNotFoundException( id )
-	
-		ret = env.get_template('profile.html').render( player=player )
-		session.close()
-		return ret
+			raise ElementNotFoundException( id )
 
-	except Exception, m:
-		return env.get_template('error.html').render(err_msg=str(m))
+	ret = env.get_template('profile.html').render( player=player )
+	session.close()
+	return ret
 
