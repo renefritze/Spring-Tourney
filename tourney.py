fd257f476693c28f300792ac14767d7f6654a2e9
Rene Milk <koshi@springlobby.info>
2010-06-06 21:15:07 +0200

    turn exception handling back on

--- tourney.py
+++ tourney.py
@@ -3,7 +3,6 @@ from bottle import route,request,send_file
 from siteglobals import env, db, config
 from utils import *
 from backend import Match,Tourney
-import jsonpickle,os
 
 @route('/tourneys/:filename')
 def tourney_js(filename):
@@ -12,21 +11,21 @@ def tourney_js(filename):
 
 @route('/details', method='GET')
 def output():
-	#try:
-	session = db.sessionmaker()
-	id = getSingleField( 'id', request )
-	if not id:
-		raise ElementNotFoundException( id )
-	tourney = session.query( Tourney ).filter( Tourney.id == id ).first()
-	if not tourney:
-		raise ElementNotFoundException( id )
+	try:
+		session = db.sessionmaker()
+		id = getSingleField( 'id', request )
+		if not id:
+			raise ElementNotFoundException( id )
+		tourney = session.query( Tourney ).filter( Tourney.id == id ).first()
+		if not tourney:
+			raise ElementNotFoundException( id )
 
-	q_matches = session.query( Match ).filter( Match.tourney_id == id )
-	
-	ret = env.get_template('tourney.html').render( tourney=tourney )
-	session.close()
-	return ret
+		q_matches = session.query( Match ).filter( Match.tourney_id == id )
+		
+		ret = env.get_template('tourney.html').render( tourney=tourney )
+		session.close()
+		return ret
 
-	#except Exception, m:
-		#return env.get_template('error.html').render(err_msg=str(m))
+	except Exception, m:
+		return env.get_template('error.html').render(err_msg=str(m))
 
